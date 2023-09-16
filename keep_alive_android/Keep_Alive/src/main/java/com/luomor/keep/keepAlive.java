ad1b8a62cb8f979756b29e8e2492be45dd1a4eb8
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-09-16 16:46:42 +0800

    update

--- keep_alive_android/Keep_Alive/src/main/java/com/luomor/keep/keepAlive.java
+++ keep_alive_android/Keep_Alive/src/main/java/com/luomor/keep/keepAlive.java
@@ -24,24 +24,25 @@ import io.dcloud.feature.uniapp.common.UniModule;
 public class keepAlive extends UniModule {
 
     String TAG = "KEEP_ALIVE";
+
     @UniJSMethod(uiThread = true)
     public void start(JSONObject options, UniJSCallback callback) throws JSONException {
         try {
-            Log.e(TAG, "testAsyncFunc--"+options);
-            if(mUniSDKInstance != null && mUniSDKInstance.getContext() instanceof Activity) {
+            Log.e(TAG, "testAsyncFunc--" + options);
+            if (mUniSDKInstance != null && mUniSDKInstance.getContext() instanceof Activity) {
                 Intent intent = new Intent(mUniSDKInstance.getContext(), ForegroundService.class);
-                intent.putExtra("title",options.getString("title"));
-                intent.putExtra("text",options.getString("text"));
+                intent.putExtra("title", options.getString("title"));
+                intent.putExtra("text", options.getString("text"));
 //                 开启前台通知服务
                 mUniSDKInstance.getContext().startService(intent);
 
 //                 开启一像素服务
-                if(!options.containsKey("onePxEnabled") || options.getBoolean("onePxEnabled")){
+                if (!options.containsKey("onePxEnabled") || options.getBoolean("onePxEnabled")) {
                     OnePxReceiver.register1pxReceiver(mUniSDKInstance.getContext());
                 }
 
 //                 添加任务
-                if(!options.containsKey("workerManager") || options.getBoolean("workerManager")) {
+                if (!options.containsKey("workerManager") || options.getBoolean("workerManager")) {
                     OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest
                             .Builder(KeepLiveWork.class)
                             .setInitialDelay(10, TimeUnit.SECONDS)
@@ -49,17 +50,17 @@ public class keepAlive extends UniModule {
                     WorkManager.getInstance(mUniSDKInstance.getContext()).enqueue(oneTimeWorkRequest);
                 }
             }
-            if(callback != null) {
+            if (callback != null) {
                 JSONObject data = new JSONObject();
                 data.put("code", 1);
                 callback.invoke(data);
                 //callback.invokeAndKeepAlive(data);
             }
-        }catch (Exception e){
-            if(callback != null) {
+        } catch (Exception e) {
+            if (callback != null) {
                 JSONObject data = new JSONObject();
                 data.put("code", 2);
-                data.put("err",e.getMessage());
+                data.put("err", e.getMessage());
                 callback.invoke(data);
                 //callback.invokeAndKeepAlive(data);
             }
@@ -67,15 +68,15 @@ public class keepAlive extends UniModule {
     }
 
     @UniJSMethod(uiThread = false)
-    public void destroy(UniJSCallback callback){
-        if(mUniSDKInstance != null && mUniSDKInstance.getContext() instanceof Activity) {
+    public void destroy(UniJSCallback callback) {
+        if (mUniSDKInstance != null && mUniSDKInstance.getContext() instanceof Activity) {
             Intent intent = new Intent(mUniSDKInstance.getContext(), ForegroundService.class);
             mUniSDKInstance.getContext().stopService(intent);
 
             OnePxReceiver.unregister1pxReceiver(mUniSDKInstance.getContext());
             KeepAliveJobService.stopJob(mUniSDKInstance.getContext());
         }
-        if(callback != null) {
+        if (callback != null) {
             JSONObject data = new JSONObject();
             data.put("code", 0);
             callback.invoke(data);
